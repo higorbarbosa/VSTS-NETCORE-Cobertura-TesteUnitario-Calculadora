13d07d31cb3cd31d0395c481e42a78979ff3cd7a
Higor Barbosa <hlbarbosa@stefanini.com>
2018-08-26 15:27:56 -0300

    Code Coverage

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -8,7 +8,7 @@ namespace Calculadora
         {
 
             var _operacao = new Operacoes();
-          
+
             Console.WriteLine("Somar: " + String.Format("Resultado da Soma: {0} " , _operacao.Somar(8, 2).ToString()));
             Console.WriteLine("Diminuir: " + String.Format("Resultado da Soma: {0} ", _operacao.Diminuir(8, 2).ToString()));
             Console.WriteLine("Dividir: " + String.Format("Resultado da Soma: {0} ", _operacao.Dividir(8, 2).ToString()));
@@ -17,6 +17,5 @@ namespace Calculadora
             Console.WriteLine("Pressione ENTER para Fechar...");
             Console.ReadLine();
         }
-
     }
 }
